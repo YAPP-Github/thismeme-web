0ce5232bcbc82b963e1aae36007a9c9c8093a402
YoommY <isho48239@gmail.com>
2022-12-14 21:58:52 +0900

    📦 Chore : 리뷰 반영

--- src/components/search/SearchInput/index.tsx
+++ src/components/search/SearchInput/index.tsx
@@ -10,7 +10,7 @@ function SearchInput({ onReset, ...rest }: Props) {
   return (
     <div className="relative mx-4 flex justify-start">
       <InputBase
-        className={`h-43 w-full rounded-22 bg-light-gray-10 pl-22 text-semi-bold text-dark-gray-10 placeholder-gray-10 outline-none`}
+        className="h-43 w-full rounded-22 bg-light-gray-10 pl-22 text-semi-bold text-dark-gray-10 outline-none placeholder:text-gray-10"
         {...rest}
         endComponents={
           <>
