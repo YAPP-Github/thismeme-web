e913318315bfd70beda3980c3b81c4137e4de7c5
YoommY <isho48239@gmail.com>
2022-12-22 20:58:05 +0900

    🚧 test : 태그 api fetch refactor ~ing [YW-63]

--- src/application/hooks/api/search/index.ts
+++ src/application/hooks/api/search/index.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 
 import { getRecentSearch } from "@/infra/api/search";
@@ -7,12 +7,16 @@ import type { SearchResult } from "@/types";
 export const useSearchResult = (value: string) => {
   const [searchResults, setSearchResults] = useState<SearchResult[]>();
 
+  const queryClient = useQueryClient();
   const { data, ...rest } = useQuery({
     queryKey: ["search", value],
     queryFn: () => getRecentSearch(value),
     suspense: false,
     onSuccess: (data) => {
       data.tags.length && setSearchResults(data.tags);
+      queryClient.setQueryData(["search", value], (old: any) => {
+        console.log("old", old);
+      });
     },
   });
 
