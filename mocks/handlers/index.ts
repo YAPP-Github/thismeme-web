d4ac4198c507b0a0b072b45003e99cd441bffabf
YoommY <isho48239@gmail.com>
2022-12-26 22:33:06 +0900

    ✨ feat : msw 인기 태그 api [YW-103]

--- mocks/handlers/index.ts
+++ mocks/handlers/index.ts
@@ -1,5 +1,11 @@
 import * as auth from "./auth";
 import * as post from "./post";
 import * as search from "./search";
+import * as tags from "./tags";
 
-export const handlers = [...Object.values(auth), ...Object.values(post), ...Object.values(search)];
+export const handlers = [
+  ...Object.values(auth),
+  ...Object.values(post),
+  ...Object.values(search),
+  ...Object.values(tags),
+];
---
+++ mocks/handlers/tags/index.ts
@@ -0,0 +1,41 @@
+import { rest } from "msw";
+
+export const getPopularTag = rest.get("/tags/popular", async (req, res, ctx) => {
+  return res(
+    ctx.status(200),
+    ctx.json({
+      tags: [
+        {
+          tagId: 1,
+          name: "무",
+          categoryName: "sample",
+          viewCount: 3,
+        },
+        {
+          tagId: 2,
+          name: "무한",
+          categoryName: "sample",
+          viewCount: 3,
+        },
+        {
+          tagId: 3,
+          name: "무한도",
+          categoryName: "sample",
+          viewCount: 3,
+        },
+        {
+          tagId: 4,
+          name: "무한도전",
+          categoryName: "sample",
+          viewCount: 3,
+        },
+        {
+          tagId: 5,
+          name: "무한도전전전전",
+          categoryName: "sample",
+          viewCount: 3,
+        },
+      ],
+    }),
+  );
+});
