dc30d6163627acafab09884a27a56d65373c8dee
YoommY <isho48239@gmail.com>
2023-02-25 23:12:58 +0900

    🐛 fix - 프로필 모달에 스켈레톤 추가 - 사이드바 SearchInput 에 inputMode none 추가 - 공유 svg 수정 - 필요없는 이미지 파일 삭제 [QA-2]

--- src/components/common/Navigation/Profile/Profile.tsx
+++ src/components/common/Navigation/Profile/Profile.tsx
@@ -3,17 +3,25 @@ import { Button } from "@/components/common/Button";
 import { Icon } from "@/components/common/Icon";
 import { ProfileModal, useSignUpModalContext } from "@/components/common/Modal";
 
+import { Skeleton } from "../../Skeleton";
+
 export const Profile = () => {
   const { isLoading, isLogin } = useAuth();
   const modalProps = useSignUpModalContext();
 
-  if (isLoading) return null;
   if (isLogin) return <ProfileModal />;
 
   return (
     <>
       <Button className="h-32 w-32" onClick={modalProps.onOpen}>
-        <Icon name="notloginprofile" />
+        {isLoading ? (
+          <Skeleton
+            style={{ width: "3.2rem", height: "3.2rem", borderRadius: "1rem" }}
+            variant="rectangular"
+          />
+        ) : (
+          <Icon name="notloginprofile" />
+        )}
       </Button>
     </>
   );
